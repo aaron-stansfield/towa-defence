a88dc4fa605248ded18179851bff320413b0c1be
aaron-stansfield <aaronmstansfield@gmail.com>
2025-01-30 16:01:02 +0000

    navigation

--- Assets/enmy_scrip.cs
+++ Assets/enmy_scrip.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class enmy_scrip : MonoBehaviour
 {
+    public GameObject goal;
+    private NavMeshAgent agent;
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
-        
+        goal = GameObject.FindGameObjectWithTag("goal");
+        agent = this.GetComponent<NavMeshAgent>();
+        agent.SetDestination(goal.transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveEnmy();
+    }
+
+    public void moveEnmy()
+    {
+        agent.SetDestination(goal.transform.position);
     }
 }
